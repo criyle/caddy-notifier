0a6c67a404bf6826ffea756f9289f16fc72a546d
criyle <gaoyang4425@gmail.com>
2025-05-03 19:05:12 -0400

    notifier: support header_up & header_down

--- module.go
+++ module.go
@@ -6,12 +6,14 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp/headers"
 	"github.com/dustin/go-humanize"
 	"github.com/gorilla/websocket"
 	"go.uber.org/zap"
@@ -29,13 +31,14 @@ func init() {
 // controller service
 type WebSocketNotifier struct {
 	// Upstream address for the backend controller
-	Upstream       string         `json:"upstream,omitempty"`
-	WriteWait      caddy.Duration `json:"write_wait,omitempty"`
-	PongWait       caddy.Duration `json:"pong_wait,omitempty"`
-	PingInterval   caddy.Duration `json:"ping_interval,omitempty"`
-	MaxMessageSize int64          `json:"max_message_size,omitempty"`
-	ChanSize       int            `json:"chan_size,omitempty"`
-	RecoverWait    caddy.Duration `json:"recover_wait,omitempty"`
+	Upstream       string           `json:"upstream,omitempty"`
+	WriteWait      caddy.Duration   `json:"write_wait,omitempty"`
+	PongWait       caddy.Duration   `json:"pong_wait,omitempty"`
+	PingInterval   caddy.Duration   `json:"ping_interval,omitempty"`
+	MaxMessageSize int64            `json:"max_message_size,omitempty"`
+	ChanSize       int              `json:"chan_size,omitempty"`
+	RecoverWait    caddy.Duration   `json:"recover_wait,omitempty"`
+	Headers        *headers.Handler `json:"headers,omitempty"`
 
 	// websocket upgrader
 	upgrader *websocket.Upgrader
@@ -96,6 +99,12 @@ func (m *WebSocketNotifier) Provision(ctx caddy.Context) error {
 	if chanSize == 0 {
 		chanSize = defaultChanSize
 	}
+	if m.Headers != nil {
+		err := m.Headers.Provision(ctx)
+		if err != nil {
+			return fmt.Errorf("provisioning embedded headers handler: %v", err)
+		}
+	}
 
 	m.ctx = ctx
 	m.logger = ctx.Logger()
@@ -135,6 +144,14 @@ func (m *WebSocketNotifier) Validate() error {
 
 // ServeHTTP implements caddyhttp.MiddlewareHandler.
 func (m *WebSocketNotifier) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	repl := r.Context().Value(caddy.ReplacerCtxKey).(*caddy.Replacer)
+	if m.Headers != nil && m.Headers.Request != nil {
+		m.Headers.Request.ApplyToRequest(r)
+	}
+	if m.Headers != nil && m.Headers.Response != nil {
+		m.Headers.Response.ApplyTo(w.Header(), repl)
+	}
+
 	conn, err := m.upgrader.Upgrade(w, r, w.Header())
 	if err != nil {
 		return err
@@ -149,13 +166,16 @@ func (m *WebSocketNotifier) ServeHTTP(w http.ResponseWriter, r *http.Request, ne
 // UnmarshalCaddyfile implements caddyfile.Unmarshaler. Syntax:
 //
 //	websocket_notifier [<matchers>] [<upstream>] {
-//	    # configurations
-//	    write_wait       <interval>
-//	    pong_wait        <interval>
-//	    ping_interval    <interval>
-//	    max_message_size <size>
-//	    chan_size        <num>
-//	    recovery_wait    <interval>
+//	  # configurations
+//	  write_wait       <interval>
+//	  pong_wait        <interval>
+//	  ping_interval    <interval>
+//	  max_message_size <size>
+//	  chan_size        <num>
+//	  recovery_wait    <interval>
+//
+//	  header_up   [+|-]<field> [<value|regexp> [<replacement>]]
+//	  header_down [+|-]<field> [<value|regexp> [<replacement>]]
 //	}
 func (m *WebSocketNotifier) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 	d.Next() // consume directive name
@@ -232,6 +252,74 @@ func (m *WebSocketNotifier) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 				return d.Errf("bad duration value %s: %v", d.Val(), err)
 			}
 			m.RecoverWait = caddy.Duration(dur)
+
+			// from https://github.com/caddyserver/caddy/blob/master/modules/caddyhttp/reverseproxy/caddyfile.go#L711
+		case "header_up":
+			var err error
+
+			if m.Headers == nil {
+				m.Headers = new(headers.Handler)
+			}
+			if m.Headers.Request == nil {
+				m.Headers.Request = new(headers.HeaderOps)
+			}
+			args := d.RemainingArgs()
+
+			switch len(args) {
+			case 1:
+				err = headers.CaddyfileHeaderOp(m.Headers.Request, args[0], "", nil)
+			case 2:
+				// some lint checks, I guess
+				if strings.EqualFold(args[0], "host") && (args[1] == "{hostport}" || args[1] == "{http.request.hostport}") {
+					caddy.Log().Named("caddyfile").Warn("Unnecessary header_up Host: the reverse proxy's default behavior is to pass headers to the upstream")
+				}
+				if strings.EqualFold(args[0], "x-forwarded-for") && (args[1] == "{remote}" || args[1] == "{http.request.remote}" || args[1] == "{remote_host}" || args[1] == "{http.request.remote.host}") {
+					caddy.Log().Named("caddyfile").Warn("Unnecessary header_up X-Forwarded-For: the reverse proxy's default behavior is to pass headers to the upstream")
+				}
+				if strings.EqualFold(args[0], "x-forwarded-proto") && (args[1] == "{scheme}" || args[1] == "{http.request.scheme}") {
+					caddy.Log().Named("caddyfile").Warn("Unnecessary header_up X-Forwarded-Proto: the reverse proxy's default behavior is to pass headers to the upstream")
+				}
+				if strings.EqualFold(args[0], "x-forwarded-host") && (args[1] == "{host}" || args[1] == "{http.request.host}" || args[1] == "{hostport}" || args[1] == "{http.request.hostport}") {
+					caddy.Log().Named("caddyfile").Warn("Unnecessary header_up X-Forwarded-Host: the reverse proxy's default behavior is to pass headers to the upstream")
+				}
+				err = headers.CaddyfileHeaderOp(m.Headers.Request, args[0], args[1], nil)
+			case 3:
+				err = headers.CaddyfileHeaderOp(m.Headers.Request, args[0], args[1], &args[2])
+			default:
+				return d.ArgErr()
+			}
+
+			if err != nil {
+				return d.Err(err.Error())
+			}
+
+		case "header_down":
+			var err error
+
+			if m.Headers == nil {
+				m.Headers = new(headers.Handler)
+			}
+			if m.Headers.Response == nil {
+				m.Headers.Response = &headers.RespHeaderOps{
+					HeaderOps: new(headers.HeaderOps),
+				}
+			}
+			args := d.RemainingArgs()
+
+			switch len(args) {
+			case 1:
+				err = headers.CaddyfileHeaderOp(m.Headers.Response.HeaderOps, args[0], "", nil)
+			case 2:
+				err = headers.CaddyfileHeaderOp(m.Headers.Response.HeaderOps, args[0], args[1], nil)
+			case 3:
+				err = headers.CaddyfileHeaderOp(m.Headers.Response.HeaderOps, args[0], args[1], &args[2])
+			default:
+				return d.ArgErr()
+			}
+
+			if err != nil {
+				return d.Err(err.Error())
+			}
 
 		default:
 			return d.Errf("unrecognized subdirective %s", d.Val())
@@ -338,6 +426,14 @@ func updateMetrics(hub *messageHub, upstream string) {
 	hub.subscribeRequested = 0
 	caddyNotifierMetrics.activeConnection.WithLabelValues(upstream).Set(float64(len(hub.websocketChannel)))
 	caddyNotifierMetrics.channelCount.WithLabelValues(upstream).Set(float64(len(hub.channels)))
+}
+
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
 }
 
 // Interface guards
